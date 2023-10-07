e00cc08c535e8f750196b5aea3e07e69ab4905a9
Ren Wang <iamwrm@gmail.com>
2023-10-07 10:26:23 +0800

    add openssl

---
+++ scripts/a.sh
@@ -0,0 +1,17 @@
+cat > message.txt <<EOF
+a message
+EOF
+
+OPENSSL_PARAM="-aes-256-cbc -pbkdf2"
+
+openssl enc $OPENSSL_PARAM \
+    -in message.txt \
+    -out message.enc \
+    -k password
+
+echo "encrypted message:"
+cat message.enc 
+
+echo -e "\ndecrepted message:"
+
+cat message.enc | openssl enc -d $OPENSSL_PARAM -k password
