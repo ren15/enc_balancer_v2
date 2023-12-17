71cf2daa23a72e467f3a40ff6b2eedd9c4a450e7
iamwrm <iamwrm@gmail.com>
2023-12-17 13:00:55 +0800

    Update unarchive_local_test.sh

--- scripts/unarchive_local_test.sh
+++ scripts/unarchive_local_test.sh
@@ -4,7 +4,6 @@ set -ueo pipefail
 
 READ_ENV_URL=https://raw.githubusercontent.com/iamwrm/read_env/main/read_env.sh
 # run read_env.sh without saving it
-export OPENSSL_ENC_KEY=$(bash <(curl -s $READ_ENV_URL) \
-    .env OPENSSL_ENC_KEY)
+export OPENSSL_ENC_KEY=$(bash <(curl -s $READ_ENV_URL) .env OPENSSL_ENC_KEY)
 
 bash scripts/unarchive.sh
